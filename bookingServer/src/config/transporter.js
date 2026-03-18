34a1a5e158df8065fa3126a21c3ca665f101f33d
Garv Singla <garvs0041@gmail.com>
2026-03-18 19:53:05 +0530

    Updated Files

--- bookingServer/src/config/transporter.js
+++ bookingServer/src/config/transporter.js
@@ -1,13 +1,13 @@
-const nodemailer = require('nodemailer');
-const { EMAIL_USER, EMAIL_PASS } = require('./serverConfig');
+// const nodemailer = require('nodemailer');
+// const { EMAIL_USER, EMAIL_PASS } = require('./serverConfig');
 
-// Create a transporter object using the default SMTP transport
-const transporter = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-      user: EMAIL_USER,
-      pass: EMAIL_PASS
-  }
-});
+// // Create a transporter object using the default SMTP transport
+// const transporter = nodemailer.createTransport({
+//   service: 'Gmail',
+//   auth: {
+//       user: EMAIL_USER,
+//       pass: EMAIL_PASS
+//   }
+// });
 
-module.exports = transporter
+// module.exports = transporter
